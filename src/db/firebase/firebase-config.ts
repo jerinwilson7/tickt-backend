e210ed6016c1afd52cf0a887e0d5b249c4f454db
JerinWilson <jerin.wilson@vonnue.com>
2025-02-01 16:55:29 +0530

    update package.json

--- src/db/firebase/firebase-config.ts
+++ src/db/firebase/firebase-config.ts
@@ -3,6 +3,8 @@ import { FIREBASE_SERVICE_ACCOUNT } from "../../constants";
 
 const serviceAccountString = atob(FIREBASE_SERVICE_ACCOUNT?.toString() ?? "");
 
+console.log("SERCICE :",serviceAccountString)
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(serviceAccountString)),
