835905ead24bfc9b6d8ced99d208c87839c1da09
JerinWilson <jerin.wilson@vonnue.com>
2025-02-01 16:37:05 +0530

    refactor: firebase

--- src/db/firebase/firebase-config.ts
+++ src/db/firebase/firebase-config.ts
@@ -1,9 +1,26 @@
+
 import { FIREBASE_SERVICE_ACCOUNT } from "../../constants";
-import * as admin from 'firebase-admin'
-export const serviceAccountString = atob(FIREBASE_SERVICE_ACCOUNT?.toString() ?? "");
+import * as admin from "firebase-admin";
 
-admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(serviceAccountString)),
-});
+if (!FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is not defined.");
+}
 
-export default admin
+try {
+  console.log("FIREBASE_SERVICE_ACCOUNT:", FIREBASE_SERVICE_ACCOUNT);
+
+  const serviceAccountString = atob(FIREBASE_SERVICE_ACCOUNT.toString());
+  console.log("Decoded serviceAccountString:", serviceAccountString);
+
+  const serviceAccount = JSON.parse(serviceAccountString);
+  console.log("Parsed serviceAccount:", serviceAccount);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} catch (error) {
+  console.error("Error initializing Firebase Admin SDK:", error);
+  throw error; // Re-throw the error to stop the application
+}
+
+export default admin;
